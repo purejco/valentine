78a98b03ed92a244387b864c89ef4e0f0eb24d4b
purejco <josue.olvera339@gmail.com>
2026-02-11 13:13:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener("touchstart", startMusic, { once: true });
 
 yesBtn.addEventListener("click", () => {
   startMusic(); // ensure music starts on YES
-  msg.textContent = "guys she said YES LESSS GOO💞 okay date night is a lock,i wuv you<3";
+  msg.textContent = "guys she said YES LESSS GOO💞 okay date night is a lock, i wuv you<3";
   confettiHearts();
 });
 
